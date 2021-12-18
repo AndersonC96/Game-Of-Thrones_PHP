c319a39e54d87f73f5ecfad0a4a3f244acf0cb4f
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2021-12-18 15:59:57 -0300

    Update house_details.php

--- house_details.php
+++ house_details.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <title>A Song of Ice and Fire | Houses</title>
+        <title>As Crônicas de Gelo e Fogo | Casas</title>
         <link rel="shortcut icon" href="./favicon.ico"/>
         <embed name="myMusic" loop="true" hidden="true" src="./music.mp3">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
@@ -23,24 +23,25 @@
         ?>
         <div class="container">
             <ul class="nav nav-tabs">
-                <li><a href="index.php">Books</a></li>
-                <li class="active"><a href="houses.php">Houses</a></li>
-                <li><a href="characters.php">Characters</a></li>
+                <li><a href="index.php">Livros</a></li>
+                <li class="active"><a href="houses.php">Casas</a></li>
+                <li><a href="characters.php">Personagens</a></li>
             </ul>
-            <h1>House Details</h1>
+            <h1>Detalhes da casa</h1>
             <div class="table-bordered">
                 <table class="table">
                     <?php
-                        echo "<tr><td><span>Name</span></td><td>".$json["name"]."</td></tr>";
-                        echo "<tr><td>Region</td><td>".$json["region"]."</td></tr>";
-                        echo "<tr><td>Coat Of Arms</td><td>".$json["coatOfArms"]."</td></tr>";
-                        echo "<tr><td>Words</td><td>".$json["words"]."</td></tr>";
-                        echo "<tr><td>Titles</td><td>".implode(", ",$json["titles"])."</td></tr>";
-                        echo "<tr><td>Seats</td><td>".implode(", ",$json["seats"])."</td></tr>";
-                        echo "<tr><td>Founded</td><td>".$json["founded"]."</td></tr>";
-                        echo "<tr><td>Died Out</td><td>".$json["diedOut"]."</td></tr>";
-                        echo "<tr><td>Ancestral Weapons</td><td>".implode(", ",$json["ancestralWeapons"])."</td></tr>";
-                        echo "<tr><td>Cadet Branches</td><td>".implode(", ",$json["cadetBranches"])."</td></tr>";
+                        echo "<tr><td><span><b>Nome</b></span></td><td>".$json["name"]."</td></tr>";
+                        echo "<tr><td><b>Região</b></td><td>".$json["region"]."</td></tr>";
+                        echo "<tr><td><b>Brazão</b></td><td>".$json["coatOfArms"]."</td></tr>";
+                        echo "<tr><td><b>Words</b></td><td>".$json["words"]."</td></tr>";
+                        echo "<tr><td><b>Títulos</b></td><td>".implode(", ",$json["titles"])."</td></tr>";
+                        echo "<tr><td><b>Seats</b></td><td>".implode(", ",$json["seats"])."</td></tr>";
+                        echo "<tr><td><b>Criação</b></td><td>".$json["founded"]."</td></tr>";
+                        echo "<tr><td><b>Fundador</b></td><td>".$json["founder"]."</td></tr>";
+                        echo "<tr><td><b>Extinção</b></td><td>".$json["diedOut"]."</td></tr>";
+                        echo "<tr><td><b>Armas Ancestrais</b></td><td>".implode(", ",$json["ancestralWeapons"])."</td></tr>";
+                        //echo "<tr><td>Cadet Branches</td><td>".implode(", ",$json["cadetBranches"])."</td></tr>";
                     ?>
                 </table>
             </div>

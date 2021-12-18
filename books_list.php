4a3e6b6797c1d8f10425cf736efed4de23f6f8db
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2021-12-18 15:33:47 -0300

    Update books_list.php

--- books_list.php
+++ books_list.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <title>A Song of Ice and Fire | Books/title>
+        <title>As Crônicas de Gelo e Fogo | Livros</title>
         <link rel="shortcut icon" href="./favicon.ico"/>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>

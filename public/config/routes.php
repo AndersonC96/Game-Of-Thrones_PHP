6911d8473a03a5eff7da96a0653279a04c29ec32
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-10-09 17:12:38 -0300

    Create routes.php

---
+++ public/config/routes.php
@@ -0,0 +1,19 @@
+<?php
+    $routes = [
+        '/characters' => 'CharacterController@index',
+        '/characters/{id}' => 'CharacterController@show',
+        '/houses' => 'HouseController@index',
+        '/houses/{id}' => 'HouseController@show',
+        '/books' => 'BookController@index',
+        '/books/{id}' => 'BookController@show',
+    ];
+    function resolveRoute($uri, $routes) {
+        foreach ($routes as $route => $controllerAction) {
+            $pattern = preg_replace('/\{[^\}]+\}/', '(\d+)', $route);
+            if (preg_match("#^$pattern$#", $uri, $matches)) {
+                array_shift($matches);
+                return [$controllerAction, $matches];
+            }
+        }
+        return null;
+    }

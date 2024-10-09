7c114a599c74fa797fa09e7d42bb895ddacfa31f
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-10-09 18:19:13 -0300

    Update header.php

---
+++ public/app/views/layouts/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Game of Thrones</title>
+        <link rel="stylesheet" href="/assets/css/styles.css">
+    </head>
+    <body>
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="/characters">Personagens</a></li>
+                    <li><a href="/houses">Casas</a></li>
+                    <li><a href="/books">Livros</a></li>
+                </ul>
+            </nav>
+        </header>

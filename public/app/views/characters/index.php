b6f3a8188eb35c5de6ba85b7ced2bb7971b3be70
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-10-10 10:58:37 -0300

    Update index.php

--- public/app/views/characters/index.php
+++ public/app/views/characters/index.php
@@ -1,5 +1,9 @@
 <?php include '../app/views/layouts/header.php'; ?>
 <h1>Personagens de Game of Thrones</h1>
+<form method="GET" action="/characters">
+    <input type="text" name="search" placeholder="Digite o nome do personagem..." value="<?php echo isset($_GET['search']) ? htmlspecialchars($_GET['search']) : ''; ?>">
+    <button type="submit">Buscar</button>
+</form>
 <?php if (count($characters) > 0): ?>
     <ul>
         <?php foreach ($characters as $character): ?>

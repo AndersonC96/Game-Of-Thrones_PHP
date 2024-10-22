28200e6a39923a79a71ce40a739b06521941f58f
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-10-22 18:23:12 -0300

    Update BookController.php

--- app/Controllers/BookController.php
+++ app/Controllers/BookController.php
@@ -4,20 +4,21 @@
     class BookController {
         // Método para exibir a lista de livros
         public function index() {
-            // Obtém todos os livros do modelo Book
             $books = Book::getAllBooks();
-            // Define o caminho da view para listar os livros
             $content = '../app/Views/books/index.php';
-            // Chama o layout principal com o conteúdo da view
             require_once '../app/Views/layouts/layout.php';
         }
         // Método para exibir os detalhes de um livro específico
-        public function details($id) {
-            // Obtém o livro com base no ID
-            $book = Book::getBookById($id);
-            // Define o caminho da view para exibir os detalhes do livro
-            $content = '../app/Views/books/details.php';
-            // Chama o layout principal com o conteúdo da view
-            require_once '../app/Views/layouts/layout.php';
+        public function details() {
+            // Obtenha o ID via GET
+            $id = $_GET['id'] ?? null;
+            if ($id) {
+                // Obtém os detalhes do livro com base no ID
+                $book = Book::getBookById($id);
+                $content = '../app/Views/books/details.php';
+                require_once '../app/Views/layouts/layout.php';
+            } else {
+                echo "ID do livro não foi fornecido.";
+            }
         }
     }

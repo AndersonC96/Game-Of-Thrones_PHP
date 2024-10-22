725483f6e34d21b8319e6f99f09c2423fffab83a
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-10-22 16:43:24 -0300

    Create footer.php

---
+++ app/Views/layouts/footer.php
@@ -0,0 +1,10 @@
+        </div>
+        <footer class="footer mt-auto py-3 bg-dark">
+            <div class="container">
+                <span class="text-muted">Desenvolvido com ❤️ para fãs de Game of Thrones</span>
+            </div>
+        </footer>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="/js/scripts.js"></script>
+    </body>
+</html>

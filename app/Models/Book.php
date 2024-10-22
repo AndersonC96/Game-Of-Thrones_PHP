d9c73504d0ce3c0758acab9bb37bba1fa9b283b4
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-10-22 18:22:02 -0300

    Update Book.php

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,7 +4,7 @@
         // Obtém a lista de todos os livros da API
         public static function getAllBooks() {
             // URL da API para buscar todos os livros
-            $url = 'https://api.gameofthrones.com/books'; // Substitua pela URL real da API
+            $url = 'https://www.anapioficeandfire.com/api/books'; // Substitua pela URL real da API
             // Faz a requisição GET para a API
             $json = file_get_contents($url);
             // Decodifica o JSON retornado e o transforma em um array associativo
@@ -13,7 +13,7 @@
         // Obtém os detalhes de um livro específico pelo ID
         public static function getBookById($id) {
             // URL da API para buscar os detalhes de um livro específico pelo ID
-            $url = 'https://api.gameofthrones.com/books/' . $id; // Substitua pela URL real da API
+            $url = 'https://www.anapioficeandfire.com/api/books/' . $id; // Substitua pela URL real da API
             // Faz a requisição GET para a API
             $json = file_get_contents($url);
             // Decodifica o JSON retornado e o transforma em um array associativo

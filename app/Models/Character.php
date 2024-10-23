be010be9d1f1585dca74c4c5f0531d1f36be7e0e
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-10-23 16:10:31 -0300

    Update Character.php

--- app/Models/Character.php
+++ app/Models/Character.php
@@ -1,22 +1,34 @@
 <?php
     namespace App\Models;
     class Character {
-        // Obtém a lista de todos os personagens da API
-        public static function getAllCharacters() {
-            // URL da API para buscar todos os personagens
-            $url = 'https://www.anapioficeandfire.com/api/characters'; // Substitua pela URL real da API
-            // Faz a requisição GET para a API
+        public static function getAllCharacters($page = 1, $limit = 12) {
+            $offset = ($page - 1) * $limit;
+            $url = 'https://www.anapioficeandfire.com/api/characters?page=' . $page . '&pageSize=' . $limit;
             $json = file_get_contents($url);
-            // Decodifica o JSON retornado e o transforma em um array associativo
-            return json_decode($json, true);
+            $characters = json_decode($json, true);
+            // Para cada personagem, vamos buscar o nome da casa
+            foreach ($characters as &$character) {
+                if (!empty($character['allegiances'])) {
+                    foreach ($character['allegiances'] as &$houseUrl) {
+                        $houseUrl = self::getHouseName($houseUrl);
+                    }
+                }
+            }
+            return $characters;
         }
-        // Obtém os detalhes de um personagem específico pelo ID
-        public static function getCharacterById($id) {
-            // URL da API para buscar os detalhes de um personagem específico pelo ID
-            $url = 'https://www.anapioficeandfire.com/api/characters/' . $id; // Substitua pela URL real da API
-            // Faz a requisição GET para a API
+        public static function getHouseName($url) {
+            // Faz uma requisição GET para buscar os detalhes da casa
+            $json = @file_get_contents($url);
+            if ($json === false) {
+                return 'Nome da casa não disponível';
+            }
+            $data = json_decode($json, true);
+            return $data['name'] ?? 'Nome da casa não disponível';
+        }
+        public static function searchCharacterByName($name, $page = 1, $limit = 12) {
+            $offset = ($page - 1) * $limit;
+            $url = 'https://www.anapioficeandfire.com/api/characters?name=' . urlencode($name) . '&page=' . $page . '&pageSize=' . $limit;
             $json = file_get_contents($url);
-            // Decodifica o JSON retornado e o transforma em um array associativo
             return json_decode($json, true);
         }
     }

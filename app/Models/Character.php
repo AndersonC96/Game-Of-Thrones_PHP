ecd9aa86708694e578d168a594f808615788ba7c
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-10-23 15:49:41 -0300

    Update Character.php

--- app/Models/Character.php
+++ app/Models/Character.php
@@ -4,7 +4,7 @@
         // Obtém a lista de todos os personagens da API
         public static function getAllCharacters() {
             // URL da API para buscar todos os personagens
-            $url = 'https://api.gameofthrones.com/characters'; // Substitua pela URL real da API
+            $url = 'https://www.anapioficeandfire.com/api/characters'; // Substitua pela URL real da API
             // Faz a requisição GET para a API
             $json = file_get_contents($url);
             // Decodifica o JSON retornado e o transforma em um array associativo
@@ -13,7 +13,7 @@
         // Obtém os detalhes de um personagem específico pelo ID
         public static function getCharacterById($id) {
             // URL da API para buscar os detalhes de um personagem específico pelo ID
-            $url = 'https://api.gameofthrones.com/characters/' . $id; // Substitua pela URL real da API
+            $url = 'https://www.anapioficeandfire.com/api/characters/' . $id; // Substitua pela URL real da API
             // Faz a requisição GET para a API
             $json = file_get_contents($url);
             // Decodifica o JSON retornado e o transforma em um array associativo

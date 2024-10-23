f31f63cccabf6509a4080477293410d8da049e78
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-10-23 16:46:14 -0300

    Update CharacterController.php

--- app/Controllers/CharacterController.php
+++ app/Controllers/CharacterController.php
@@ -2,18 +2,26 @@
     namespace App\Controllers;
     use App\Models\Character;
     class CharacterController {
+        // Método para listar personagens com paginação e busca
         public function index() {
-            // Verificar se existe uma busca pelo nome
             $search = $_GET['search'] ?? null;
             $page = $_GET['page'] ?? 1;
             if ($search) {
-                // Se houver uma busca, filtrar por nome
                 $characters = Character::searchCharacterByName($search, $page);
             } else {
-                // Caso contrário, obter todos os personagens
                 $characters = Character::getAllCharacters($page);
             }
             $content = '../app/Views/characters/index.php';
+            require_once '../app/Views/layouts/layout.php';
+        }
+        // Método para exibir detalhes de um personagem
+        public function details($id) {
+            // Extrai o ID numérico do final da URL
+            $id = basename($id);
+            // Obtém o personagem com base no ID
+            $character = Character::getCharacterById($id);
+            // Define o caminho da view para exibir os detalhes do personagem
+            $content = '../app/Views/characters/details.php';
             require_once '../app/Views/layouts/layout.php';
         }
     }

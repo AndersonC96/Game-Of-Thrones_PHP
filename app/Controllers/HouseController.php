4ea0b0c973178780c2dec6f3b9f7a9f4035946ff
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-10-23 18:24:29 -0300

    Update HouseController.php

--- app/Controllers/HouseController.php
+++ app/Controllers/HouseController.php
@@ -2,22 +2,38 @@
     namespace App\Controllers;
     use App\Models\House;
     class HouseController {
-        // Método para exibir a lista de casas
         public function index() {
-            // Obtém todas as casas do modelo House
-            $houses = House::getAllHouses();
-            // Define o caminho da view para listar as casas
-            $content = '../app/Views/houses/index.php';
-            // Chama o layout principal com o conteúdo da view
-            require_once '../app/Views/layouts/layout.php';
+            // Configurações de paginação
+            $housesPerPage = 12; // Exibir 12 casas por página
+            $currentPage = isset($_GET['page']) ? (int) $_GET['page'] : 1;
+            if ($currentPage < 1) {
+            $currentPage = 1;
         }
-        // Método para exibir os detalhes de uma casa específica
-        public function details($id) {
-            // Obtém a casa com base no ID
-            $house = House::getHouseById($id);
-            // Define o caminho da view para exibir os detalhes da casa
-            $content = '../app/Views/houses/details.php';
-            // Chama o layout principal com o conteúdo da view
-            require_once '../app/Views/layouts/layout.php';
+
+        // Obtém as casas da API com paginação
+        $houses = House::getAllHouses($currentPage, $housesPerPage);
+        $totalHouses = 444; // Definir manualmente, pois a API não retorna o total
+        $totalPages = ceil($totalHouses / $housesPerPage);
+
+        // Verifica se há casas retornadas
+        if (empty($houses)) {
+            $housesToShow = [];
+        } else {
+            foreach ($houses as &$house) {
+                // Busca o nome do overlord
+                if (!empty($house['overlord'])) {
+                    $house['overlordName'] = House::getHouseOrCharacterNameByUrl($house['overlord']);
+                }
+                // Busca o nome do currentLord
+                if (!empty($house['currentLord'])) {
+                    $house['currentLordName'] = House::getHouseOrCharacterNameByUrl($house['currentLord']);
+                }
+            }
+            $housesToShow = $houses;
         }
+
+        // Define o caminho da view para listar as casas
+        $content = '../app/Views/houses/index.php';
+        require_once '../app/Views/layouts/layout.php';
     }
+}

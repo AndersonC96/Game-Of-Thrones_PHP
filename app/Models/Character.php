0e55ff0f79c64ccfe02ef6be771b34642ed08054
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-10-23 16:46:18 -0300

    Update Character.php

--- app/Models/Character.php
+++ app/Models/Character.php
@@ -1,12 +1,11 @@
 <?php
     namespace App\Models;
     class Character {
+        // Método para obter todos os personagens com paginação
         public static function getAllCharacters($page = 1, $limit = 12) {
-            $offset = ($page - 1) * $limit;
             $url = 'https://www.anapioficeandfire.com/api/characters?page=' . $page . '&pageSize=' . $limit;
             $json = file_get_contents($url);
             $characters = json_decode($json, true);
-            // Para cada personagem, vamos buscar o nome da casa
             foreach ($characters as &$character) {
                 if (!empty($character['allegiances'])) {
                     foreach ($character['allegiances'] as &$houseUrl) {
@@ -16,8 +15,14 @@
             }
             return $characters;
         }
+        // Novo método para obter detalhes de um personagem pelo ID
+        public static function getCharacterById($id) {
+            $url = 'https://www.anapioficeandfire.com/api/characters/' . $id;
+            $json = file_get_contents($url);
+            return json_decode($json, true);
+        }
+        // Método para obter o nome da casa
         public static function getHouseName($url) {
-            // Faz uma requisição GET para buscar os detalhes da casa
             $json = @file_get_contents($url);
             if ($json === false) {
                 return 'Nome da casa não disponível';
@@ -26,7 +31,6 @@
             return $data['name'] ?? 'Nome da casa não disponível';
         }
         public static function searchCharacterByName($name, $page = 1, $limit = 12) {
-            $offset = ($page - 1) * $limit;
             $url = 'https://www.anapioficeandfire.com/api/characters?name=' . urlencode($name) . '&page=' . $page . '&pageSize=' . $limit;
             $json = file_get_contents($url);
             return json_decode($json, true);

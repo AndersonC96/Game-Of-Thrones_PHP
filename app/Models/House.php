886dbf63abf04a7f8edfe50b31bfde4961012680
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-10-25 18:25:47 -0300

    Update House.php

--- app/Models/House.php
+++ app/Models/House.php
@@ -16,17 +16,17 @@
             // Verifica se o retorno da API é um array válido
             return is_array($houses) ? $houses : [];
         }
-        // Método para buscar o nome de um overlord ou currentLord com base na URL
+        // Método para buscar o nome de um personagem ou casa com base na URL
         public static function getHouseOrCharacterNameByUrl($url) {
             $json = file_get_contents($url);
             if ($json === FALSE) {
                 error_log("Erro ao tentar acessar a URL: $url");
                 return 'Nome não disponível';
             }
-            // Decodifica o JSON e verifica se o campo 'name' está presente
-            $data = json_decode($json, true);
-            return !empty($data['name']) ? $data['name'] : 'Nome não disponível';
-        }
+        // Decodifica o JSON e verifica se o campo 'name' está presente
+        $data = json_decode($json, true);
+        return !empty($data['name']) ? $data['name'] : 'Nome não disponível';
+    }
         // Método para buscar detalhes de uma casa pelo URL
         public static function getHouseByUrl($url) {
             $json = file_get_contents($url);

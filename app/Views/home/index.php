d608d675f7ebf8998e3e014cd757e07b82df50cc
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-10-28 12:54:54 -0300

    Update index.php

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -6,28 +6,31 @@
 <div class="row mt-4">
     <div class="col-md-4">
         <div class="card">
+            <img src="/img/books.jpg" alt="Image cap">
             <div class="card-body">
                 <h5 class="card-title">Livros</h5>
                 <p class="card-text">Descubra os livros de Game of Thrones.</p>
-                <a href="/books" class="btn btn-primary">Ver Livros</a>
+                <a href="/books" class="btn">Ver Livros</a>
             </div>
         </div>
     </div>
     <div class="col-md-4">
         <div class="card">
+            <img src="/img/characters.jpg" alt="Image cap">
             <div class="card-body">
                 <h5 class="card-title">Personagens</h5>
                 <p class="card-text">Explore os personagens da série.</p>
-                <a href="/characters" class="btn btn-primary">Ver Personagens</a>
+                <a href="/characters" class="btn">Ver Personagens</a>
             </div>
         </div>
     </div>
     <div class="col-md-4">
         <div class="card">
+            <img src="/img/houses.jpg" alt="Image cap">
             <div class="card-body">
                 <h5 class="card-title">Casas</h5>
                 <p class="card-text">Veja as grandes casas de Westeros.</p>
-                <a href="/houses" class="btn btn-primary">Ver Casas</a>
+                <a href="/houses" class="btn">Ver Casas</a>
             </div>
         </div>
     </div>

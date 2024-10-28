97485f194c254181b265af637443e8a23e692847
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-10-28 14:21:28 -0300

    Update index.php

--- app/Views/books/index.php
+++ app/Views/books/index.php
@@ -5,10 +5,12 @@
             <div class="col-md-4">
                 <div class="card mb-4">
                     <div class="card-body">
-                        <h5 class="card-title"><?= htmlspecialchars($book['name']); ?></h5>
-                        <p class="card-text"><b>Autor</b>: <?= htmlspecialchars($book['authors'][0]); ?></p>
-                        <p class="card-text"><b>Data de lançamento</b>: <?= htmlspecialchars($book['released']) ? htmlspecialchars(date('d/m/Y', strtotime($book['released']))) : 'Data não disponível'; ?></p>
-                        <a href="/books/details?id=<?= urlencode(basename($book['url'])) ?>" class="btn btn-primary">Ver detalhes</a>
+                        <div class="card-body">
+                            <h5 class="card-title"><?= htmlspecialchars($book['name']); ?></h5>
+                            <p class="card-text"><b>Autor</b>: <?= htmlspecialchars($book['authors'][0]); ?></p>
+                            <p class="card-text"><b>Data de lançamento</b>: <?= htmlspecialchars($book['released']) ? htmlspecialchars(date('d/m/Y', strtotime($book['released']))) : 'Data não disponível'; ?></p>
+                            <a href="/books/details?id=<?= urlencode(basename($book['url'])) ?>" class="btn">Ver detalhes</a>
+                        </div>
                     </div>
                 </div>
             </div>
